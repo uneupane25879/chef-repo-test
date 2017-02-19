84b3e9dd5e70e531d49fe3a332d1f70882269ed3
aamirahmad <aamir.ahmad170@gmail.com>
2017-02-19 22:51:19 +0530

    Written the cookbook

--- cookbooks/nginx/recipes/default.rb
+++ cookbooks/nginx/recipes/default.rb
@@ -1,5 +1,12 @@
-#
-# Cookbook:: nginx
-# Recipe:: default
-#
-# Copyright:: 2017, The Authors, All Rights Reserved.
+package 'nginx' do
+  action :install
+end
+
+service 'nginx' do
+  action [ :enable, :start]
+end
+
+cookbook_file "/var/www/html/index.html" do
+  source "index.html"
+  mode "0644"
+end
