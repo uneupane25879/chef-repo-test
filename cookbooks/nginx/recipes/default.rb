27a8ee6344821943c3d3a0277449d81478a91e40
aamirahmad <aamir.ahmad170@gmail.com>
2017-02-19 23:03:28 +0530

    Changing name of index.html

--- cookbooks/nginx/recipes/default.rb
+++ cookbooks/nginx/recipes/default.rb
@@ -6,7 +6,7 @@ service 'nginx' do
   action [ :enable, :start]
 end
 
-cookbook_file "/var/www/html/index.html" do
+cookbook_file "/var/www/html/index.nginx-debian.html" do
   source "index.html"
   mode "0644"
 end
